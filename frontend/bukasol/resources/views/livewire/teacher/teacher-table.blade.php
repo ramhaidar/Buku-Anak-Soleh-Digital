e6b794091b66c67d5ba0cb024d8607b900f5e138
ramhaidar <49301219+ramhaidar@users.noreply.github.com>
2024-11-02 04:07:45 +0700

    Feat: "Export Akun" and "Tambah Siswa" Button
    - Changed favicon.ico

--- frontend/bukasol/resources/views/livewire/teacher/teacher-table.blade.php
+++ frontend/bukasol/resources/views/livewire/teacher/teacher-table.blade.php
@@ -1,7 +1,17 @@
 <div>
     @if ($showTeacherTable)
         <div class="text-center">
-            <h3 class="poppins-medium">Data Guru</h3>
+            {{-- <h3 class="poppins-medium">Data Guru</h3> --}}
+            <div class="row align-items-center mb-4">
+                <div class="container position-relative">
+                    <!-- Centered Title -->
+                    <h2 class="text-center mb-0 position-absolute top-0 start-50 translate-middle-x">Data Guru</h2>
+                </div>
+                <div class="col d-flex justify-content-end align-items-end mt-3 mt-md-0">
+                    <button class="btn btn-outline-dark rounded-3 me-2"><i class="fa-solid fa-file-contract me-1"></i> Export Akun</button>
+                    <button class="btn btn-outline-dark rounded-3"><i class="fa-solid fa-plus me-1"></i> Tambah Siswa</button>
+                </div>
+            </div>
         </div>
         <table style="width: 100%; text-align: left;" border="1" cellpadding="10">
             <thead>
