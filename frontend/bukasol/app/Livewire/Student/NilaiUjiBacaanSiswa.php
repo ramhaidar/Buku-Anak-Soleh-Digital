1940173e2024be01c2aca41940f501aa70ddd8f1
ramhaidar <49301219+ramhaidar@users.noreply.github.com>
2024-11-19 10:42:55 +0700

    Feat: Nilai Uji Bacaan Siswa [Student Dashboard]

---
+++ frontend/bukasol/app/Livewire/Student/NilaiUjiBacaanSiswa.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Student;
+
+use Livewire\Component;
+
+class NilaiUjiBacaanSiswa extends Component
+{
+    public function render ()
+    {
+        return view ( 'livewire.student.nilai-uji-bacaan-siswa' );
+    }
+}
